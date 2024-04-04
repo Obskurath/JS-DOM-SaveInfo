08f238b617c7701a9b3391d4ccb79c68ee6ce326
Eduardo Valencia <EduardoVG@proton.me>
2024-04-04 15:32:46 -0600

    Order dates

--- components/readTasks.js
+++ components/readTasks.js
@@ -1,5 +1,5 @@
 import { createTask } from "./addTask.js";
-import { uniqueDates } from "../services/date.js";
+import { uniqueDates, orderDates } from "../services/date.js";
 import dateElement from "./dateElement.js";
 
 export const displayTasks = () => {
@@ -7,14 +7,13 @@ export const displayTasks = () => {
 
     const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
     const dates = uniqueDates(taskList);
-    console.log(dates)
-
+    orderDates(dates);
     dates.forEach(date => {
         const dateMoment = moment(date, 'DD/MM/YYYY');
         list.appendChild(dateElement(date));
         taskList.forEach((task) => {
-            const taskDate = moment (task.dateFormatted, 'DD/MM/YYYY')
-            const diff = dateMoment.diff(taskDate)
+            const taskDate = moment (task.dateFormatted, 'DD/MM/YYYY');
+            const diff = dateMoment.diff(taskDate);
             if ( diff === 0) {
                 list.appendChild(createTask(task));
             }
