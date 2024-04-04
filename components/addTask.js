6ac2df45cc03e3accd34f0897d53d7c59caa2b2d
Eduardo Valencia <EduardoVG@proton.me>
2024-04-03 21:52:17 -0600

    Fixing addTask component

--- components/addTask.js
+++ components/addTask.js
@@ -1,23 +1,23 @@
 import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
 
-const addTask = (e) => {
+export const addTask = (e) => {
     const list = document.querySelector('[data-list]');
     const task = createTask(e)
     list.appendChild(task);
-  
-  
-  }
-  
-  const taskList = [];
-  
-  const createTask = (evento) => {
+}
+
+
+
+const createTask = (evento) => {
     evento.preventDefault();
-    const calendar =  document.querySelector("[data-form-date]");
+    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+    console.log(taskList)
+    const calendar = document.querySelector("[data-form-date]");
     const date = calendar.value;
     const dateFormatted = moment(date).format("DD/MM/YYYY");
     console.log(dateFormatted);
-    
+
     const input = document.querySelector('[data-form-input]');
     const value = input.value;
     const task = document.createElement('li');
@@ -26,15 +26,14 @@ const addTask = (e) => {
     //backticks
     const taskContent = document.createElement('div');
     const taskObj = {
-      value,
-      dateFormatted
+        value,
+        dateFormatted
     }
-  
-    taskList.push(taskObj)
-  
+
+    taskList.push(taskObj);
+
     localStorage.setItem('tasks', JSON.stringify(taskList));
-  
-    console.log(taskObj)
+
     const titleTask = document.createElement('span');
     titleTask.classList.add('task');
     titleTask.innerText = value;
@@ -43,9 +42,9 @@ const addTask = (e) => {
     // task.innerHTML = content;
     const dateElement = document.createElement('span')
     dateElement.innerHTML = dateFormatted
-  
+
     task.appendChild(taskContent);
     task.appendChild(dateElement)
     task.appendChild(deleteIcon());
     return task;
-  };
+};
