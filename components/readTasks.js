2c8c9bf6cc219fd0cf2140405beb141262260d60
Eduardo Valencia <EduardoVG@proton.me>
2024-04-04 14:13:42 -0600

    Fixing js

--- components/readTasks.js
+++ components/readTasks.js
@@ -1,13 +1,20 @@
 import { createTask } from "./addTask.js";
+import { uniqueDates } from "../services/date.js";
+import dateElement from "./dateElement.js";
 
 export const readTask = () => {
     const list = document.querySelector('[data-list]');
-    console.log(list);
 
     const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
-    console.log(taskList);
+    const dates = uniqueDates(taskList);
+    console.log(dates)
+
+    dates.forEach(date => {
+        console.log(date);
+    });
 
     taskList.forEach((task) => {
+        list.appendChild(dateElement(task.dateFormatted))
         list.appendChild(createTask(task));
     });
 };
